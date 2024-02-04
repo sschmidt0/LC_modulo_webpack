a334c08062a357fdc70040b22057260d0b8af917
sakschmidt <sarah.schmidt@code.seat>
2024-02-04 22:18:45 +0100

    feat: add prod configuration for css and sass

---
+++ webpack.prod.js
@@ -0,0 +1,26 @@
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import common from "./webpack.common.js";
+import { merge } from "webpack-merge";
+
+export default merge(common, {
+  mode: "production",
+  output: {
+    filename: "js/[name].[chunkhash].js",
+    assetModuleFilename: "assets/[hash][ext][query]",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[chunkhash].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
+});
