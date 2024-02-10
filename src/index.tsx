d8974f43492059ccffa06ea33260dbc27a943433
sakschmidt <sarah.schmidt@code.seat>
2024-02-10 18:52:25 +0100

    feat: add folder aliases

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { ShowGreet } from "./show-greet";
+import { Button } from "@common/components";
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
   <div>
     <ShowGreet bundlingPackage="webpack" username="Sarah" />
+    <Button onClick={() => console.log('Clicked')} text="Click me now!" />
   </div>
 );
